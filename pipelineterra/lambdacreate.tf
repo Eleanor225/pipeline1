2bb71d66c2548ec963861b5f0c5c035d1823dfaf
Eleanor <eleanor.oliver@sainsburys.co.uk>
2020-02-12 16:56:51 +0000

    correcting the source code hash issue and interpolation issue

--- pipelineterra/lambdacreate.tf
+++ pipelineterra/lambdacreate.tf
@@ -54,19 +54,25 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "attach_sqs" {
-  role = "${aws_iam_role.iam_for_lambda.name}"
-  policy_arn = "${aws_iam_policy.policy.arn}"
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+data "archive_file" "lambda_zip" {
+    type        = "zip"
+    source_file  = "../pipelineproj/lambdascript.py"
+    output_path = "lambda_zip.zip"
 }
 
 resource "aws_lambda_function" "copy_file" {
   function_name = "sns-sqs-copyfile"
-  filename = "lambdafunc.zip"
+  filename = "lambda_zip.zip"
   handler = "lambdascript.lambda_handler"
   runtime = "python3.7"
-  role = "${aws_iam_role.iam_for_lambda.arn}"
+  role = aws_iam_role.iam_for_lambda.arn
   memory_size = 1024
   timeout = 5
-  source_code_hash = "${filesha256("lambdafunc.zip")}"
+  source_code_hash = "data.archive_file.lambda_zip.output_base64sha256"
 }
 
 resource "aws_lambda_permission" "sqs" {
@@ -74,10 +80,10 @@ resource "aws_lambda_permission" "sqs" {
   action = "lambda:InvokeFunction"
   function_name = "sns-sqs-copyfile"
   principal = "sqs.amazonaws.com"
-  source_arn = "${aws_sqs_queue.queue.arn}"
+  source_arn = aws_sqs_queue.queue.arn
 }
 
 resource "aws_lambda_event_source_mapping" "lambda" {
-  event_source_arn = "${aws_sqs_queue.queue.arn}"
-  function_name = "${aws_lambda_function.copy_file.arn}"
+  event_source_arn = aws_sqs_queue.queue.arn
+  function_name = aws_lambda_function.copy_file.arn
 }
