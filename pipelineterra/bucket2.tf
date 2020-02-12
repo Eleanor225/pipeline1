058f01013ed09e378fd129976cb4c5cd0b539384
Eleanor <eleanor.oliver@sainsburys.co.uk>
2020-02-12 17:02:26 +0000

    adjusted to prevent interpolation warnings

--- pipelineterra/bucket2.tf
+++ pipelineterra/bucket2.tf
@@ -7,3 +7,21 @@ resource "aws_s3_bucket" "b" {
     Environment = "Curated"
   }
 }
+
+resource "aws_s3_bucket" "c" {
+  bucket = "terrafilestore"
+  acl = "private"
+
+  tags = {
+    Name = "File Store Bucket"
+    Environment = "File"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terrafilestore"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
