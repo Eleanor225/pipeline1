759cee8ec3bd623e652531d5210bcdde2137f10a
Eleanor <eleanor.oliver@sainsburys.co.uk>
2020-02-19 16:20:13 +0000

    added sns creation

--- pipelineterra/bucket2.tf
+++ pipelineterra/bucket2.tf
@@ -25,3 +25,35 @@ terraform {
     region = "eu-west-2"
   }
 }
+
+resource "aws_sns_topic" "curated_updates" {
+  name = "curated-updates-topic"
+
+  policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "s3.amazonaws.com"
+            },
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:curated-updates-topic",
+        "Condition":{
+            "ArnLike":{
+              "aws:SourceArn":"${aws_s3_bucket.b.arn}"
+            }
+        }
+    }]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.b.id
+
+  topic {
+    topic_arn = aws_sns_topic.curated_updates.arn
+    events = ["s3:ObjectCreated:*"]
+  }
+}
